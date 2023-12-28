f90e88b5a253b18fbb7864ab89e5d1a140334416
SukmaNigraha <sukmanigraha87@gmail.com>
2023-12-28 20:22:27 +0800

    add new index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-<h1>halo dunia</h1>
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = '<h1> hello world</h1>\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
